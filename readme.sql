1c79a04369f7e42df08e8b6d2819c462aa27cd1b
Dirceu Resende <dirceu.resende@yahoo.com.br>
2023-01-04 23:08:50 -0300

    Update readme.sql

--- readme.sql
+++ readme.sql
@@ -6,24 +6,24 @@
 DECLARE @MyParameters AS tpDataCompareParameters
 
 INSERT INTO @MyParameters (
-	DatabaseName,
-	SchemaName,
-	TableName,
-	KeyColumns
+    DatabaseName,
+    SchemaName,
+    TableName,
+    KeyColumns
 )
 VALUES
 (
-	'AdventureWorksDW2019',   -- DatabaseName - NVARCHAR(500)
-	'dbo',   -- SchemaName - NVARCHAR(500)
-	'FactInternetSales',    -- TableName - NVARCHAR(500)
-	'SalesOrderNumber, SalesOrderLineNumber'
+    'AdventureWorksDW2019',   -- DatabaseName - NVARCHAR(500)
+    'dbo',   -- SchemaName - NVARCHAR(500)
+    'FactInternetSales',    -- TableName - NVARCHAR(500)
+    'SalesOrderNumber, SalesOrderLineNumber'
 ),
 (
-	'dirceuresende',   -- DatabaseName - NVARCHAR(500)
-	'dbo',   -- SchemaName - NVARCHAR(500)
-	'FactInternetSales2',    -- TableName - NVARCHAR(500)
-	'SalesOrderNumber, SalesOrderLineNumber'
+    'dirceuresende',   -- DatabaseName - NVARCHAR(500)
+    'dbo',   -- SchemaName - NVARCHAR(500)
+    'FactInternetSales2',    -- TableName - NVARCHAR(500)
+    'SalesOrderNumber, SalesOrderLineNumber'
 )
 
 EXEC dbo.stpDataCompare 
-	@Parameters = @MyParameters  -- tpDataCompareParameters
+    @Parameters = @MyParameters  -- tpDataCompareParameters
