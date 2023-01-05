5b67b393a7a247d4770553079e79bcc7cb0a35db
Dirceu Resende <dirceu.resende@yahoo.com.br>
2023-01-04 23:06:20 -0300

    Create readme.sql

---
+++ readme.sql
@@ -0,0 +1,28 @@
+-------------------------------------------
+-- PARAMETERS (YOU CAN CHANGE HERE)
+-------------------------------------------
+
+DECLARE @MyParameters AS tpDataCompareParameters
+
+INSERT INTO @MyParameters (
+	DatabaseName,
+	SchemaName,
+	TableName,
+	KeyColumns
+)
+VALUES
+(
+	'AdventureWorksDW2019',   -- DatabaseName - NVARCHAR(500)
+	'dbo',   -- SchemaName - NVARCHAR(500)
+	'FactInternetSales',    -- TableName - NVARCHAR(500)
+	'SalesOrderNumber, SalesOrderLineNumber'
+),
+(
+	'dirceuresende',   -- DatabaseName - NVARCHAR(500)
+	'dbo',   -- SchemaName - NVARCHAR(500)
+	'FactInternetSales2',    -- TableName - NVARCHAR(500)
+	'SalesOrderNumber, SalesOrderLineNumber'
+)
+
+EXEC dbo.stpDataCompare 
+	@Parameters = @MyParameters  -- tpDataCompareParameters
